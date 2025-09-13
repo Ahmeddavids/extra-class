9135d395d9a3c9f73c4eb4600e850c7cb5aa696b
ahmeddavids <ahmeddavids6@gmail.com>
2025-09-13 17:10:15 +0100

    First Push

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('./config/database');
 const express = require('express');
 
 const app = express();
+app.use('/', (req, res) => {
+    res.send('Hello World');
+})
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
