06aa13651e81c19d9432543933d88a3a38aa8226
MustaphaHameedah <mustaphahameedah16@gmail.com>
2025-09-13 17:20:11 -0700

    express.json()

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('./config/database');
-const express = require('express');
+require("./config/database");
+const express = require("express");
 
 const app = express();
-
-app.listen(3000, ()=> {
-    console.log('Server is running on port 3000');
-})
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
